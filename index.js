d14fae0d3a061d0bd92ebf9f831f89b9d23e7dcb
Shubham Paul <paulshubham779@gmail.com>
2026-03-31 12:20:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+
+const app = express();
+
+const user = [
+    {
+        name:"Mathew",
+        Kidneys:[
+            {healthy:false},
+            {healthy:true}
+        ]
+    }
+]
+
+app.use(express.json());
+
+app.get('/', function(req, res) {
+
+    const mathewKidneys = user[0].Kidneys;
+    const noOfKidneys = mathewKidneys.length;
+
+    const mathewhealthyKidneys = mathewKidneys.filter(function(kidney){
+        return kidney.healthy == true;
+    });
+
+    const mathewunhealthyKidneys = noOfKidneys - mathewhealthyKidneys.length;
+
+    res.json({
+        healthy: mathewhealthyKidneys.length,
+        unhealthy: mathewunhealthyKidneys
+    });
+});
+
+
+app.post('/' , function(req,res){
+    const ishealthy = req.body.ishealthy;
+    user[0].Kidneys.push({
+        healthy:ishealthy
+    })
+    res.json({
+        msg:"Done"
+    })
+})
+
+//Update
+app.put('/' , function(req,res){
+    for(let i=0;i<user[0].Kidneys.length;i++){
+        user[0].Kidneys[i].healthy=true;
+    }
+    res.json({
+        msg:"Done!!"
+    })
+})
+
+//Removing all unhealthy Kidneys
+app.delete('/', function(req,res){
+    let userblankkidneys= [];
+    for(let i=0;i<user[0].Kidneys.length;i++){
+        if(user[0].Kidneys[i].healthy){
+           userblankkidneys.push({
+            healthy:true
+           })
+        }
+    }user[0].Kidneys=userblankkidneys;
+    res.json({
+        msg:"Delete Done!!"
+    })
+
+})
+
+app.listen(3000);
